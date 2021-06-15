c86a108a265a10d91aa16467d05acc7d1bc39a9f
taean34 <68094404+taean34@users.noreply.github.com>
2021-06-15 15:55:43 +0900

    Update opencv_capture.py

--- opencv_capture.py
+++ opencv_capture.py
@@ -1,34 +1,29 @@
-# pip install mediapipe opencv-python
-
+# pip install opencv-python
 import cv2
-#import mediapipe as mp
-#mp_face_detection = mp.solutions.face_detection
-#mp_drawing = mp.solutions.drawing_utils
-#mp_holistic = mp.solutions.holistic
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
 
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1024) # 크기 제한이 있는 것 같다.
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768) # 크기 제한이 있는 것 같다.
-
 while cap.isOpened():
     ret, frame = cap.read()
-    if not ret:
-      print("Ignoring empty camera frame.")
-      continue
 
-    # frame.shape (세로, 가로, 색)
+    # frame.shape (세로, 가로, 색 채널)
     h = frame.shape[0]
     w = frame.shape[1]
+    
+    # 대칭
+    flip = cv2.flip(frame, 1)
+    
+    # 일부분만 대칭
+    fliphalf = frame.copy()
+    fliphalf[0:h, w//2:w] = cv2.flip(fliphalf[0:h, 0:w//2], 1)
 
-    result = frame.copy()
-    src = frame.copy()
-    result[0:h, 0:w//2] = frame[0:h, 0:w//2]
-    result[0:h, w//2:w] = cv2.flip(frame[0:h, 0:w//2], 1)
-
-    cv2.imshow('', result)
-
+    # 보여주기
+    cv2.imshow('original', frame)
+    cv2.imshow('flip', flip)
+    cv2.imshow('fliphalf', fliphalf)
+    
+    # [ESC]키를 누르면 종료
     if cv2.waitKey(5) & 0xFF == 27:
       break
 
